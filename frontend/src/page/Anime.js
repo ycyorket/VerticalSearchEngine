ff1e8c31d14b04e794333354e37f5c8b8ef67eda
ycyorket <ycyorketernal@zju.edu.cn>
2021-07-16 19:22:52 +0800

    update

--- frontend/src/page/Anime.js
+++ frontend/src/page/Anime.js
@@ -3,8 +3,18 @@ import SearchHeader from "../component/SearchHeader";
 import AppFooter from "../component/AppFooter";
 
 const Anime = () => {
-    return (<div className = "searchpage pinkbackground">
+    return (<div className = "pinkbackground">
         <SearchHeader/>
+        <div className = "animepage">
+            <div className = "animepage-left">
+                <div className="animepage-info"/>
+                <div className="animepage-recommendation"/>
+            </div>
+            <div className = "animepage-right">
+                <div className="animepage-seiyuu"/>
+                <div className="animepage-staff"/>
+            </div>
+        </div>
         <AppFooter styleClass="normal-footer" />
     </div>)
 }
--- frontend/src/page/SorrySearch.js
+++ frontend/src/page/SorrySearch.js
@@ -3,14 +3,34 @@ import { useParams } from "react-router-dom";
 import SearchHeader from "../component/SearchHeader";
 import AppFooter from "../component/AppFooter";
 import history from "../util/history";
+import defaultPoster from "../asset/default-poster.jpeg";
 
 const NoSearchResult = (props) => {
     const keyword = props.keyword;
+    const defaultRecommendation = {
+        url: defaultPoster,
+        title: '工作细胞',
+        id: '2333'
+    }
+    const recommendations = [defaultRecommendation, defaultRecommendation, defaultRecommendation, defaultRecommendation, defaultRecommendation]
+    let i = 0;
     return (<div className="searchpage-content">
         <article className="searchpage-info">
             <h1>抱歉没有找到与“<strong>{keyword}</strong>”相关的番剧(┬┬﹏┬┬)</h1>
             <h2>温馨提示：</h2>
             <p>请检查您的输入是否正确</p>
+            <div className="recommendation">
+                <div className="recommendation-title">番剧推荐</div>
+                <div className="recommendation-content">
+                    {recommendations.map((item)=>{
+                        i += 1
+                        return (<div key={"poster"+i} className="recommendation-item" onClick={()=>{history.push(`/anime/${item.id}`)}}>
+                            <img className="recommendation-poster" src={item.url} alt={item.title+"海报"}/>
+                            <h4 className="recommendation-anime">{item.title}</h4>
+                        </div>)
+                    })}
+                </div>
+            </div>
         </article>
     </div>)
 }
